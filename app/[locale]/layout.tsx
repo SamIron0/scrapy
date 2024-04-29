2c57c0b33543eb345fb58d82dbdb9df0402e4146
SamIron0 <samuelironkwec@gmail.com>
2024-04-29 18:26:11 -0500

    x

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -105,7 +105,7 @@ export default async function RootLayout({
       <body className={inter.className}>
         <Providers attribute="class" defaultTheme="dark">
           <Toaster richColors position="top-center" duration={3000} />
-          <div className="flex h-dvh flex-col items-center overflow-x-hidden bg-background text-foreground">
+          <div className="flex flex-col items-center overflow-x-hidden bg-background text-foreground">
             {children}
           </div>
           <Analytics />
--- components/adminDashboard/dash.tsx
+++ components/adminDashboard/dash.tsx
@@ -59,7 +59,7 @@ export default function Dash() {
   return (
     <div className="my-12 flex w-full flex-col items-center p-6">
       <div className="w-full items-center  space-y-4 sm:space-x-4 md:flex ">
-        <div className=" flex size-full flex-col rounded-lg  border-2  p-4 ">
+        <div className=" flex size-full flex-col rounded-lg border-2  p-4 ">
           <Label className="text-md pb-1 pt-8" htmlFor="weight">
             Url
           </Label>
