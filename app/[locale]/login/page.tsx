efef4e42f9ed8067d1a5eafa3c059ffe55e4c1cd
SamIron0 <samuelironkwec@gmail.com>
2024-04-30 21:36:36 -0500

    z

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -54,21 +54,7 @@ export default async function Login({
     if (error) {
       return redirect(`/login?message=${error.message}`)
     }
-
-    const { data: homeWorkspace, error: homeWorkspaceError } = await supabase
-      .from("workspaces")
-      .select("*")
-      .eq("user_id", data.user.id)
-      .eq("is_home", true)
-      .single()
-
-    if (!homeWorkspace) {
-      throw new Error(
-        homeWorkspaceError?.message || "An unexpected error occurred"
-      )
-    }
-
-    return redirect(`/${homeWorkspace.id}/chat`)
+    return redirect(`/dashboard`)
   }
 
   const signUp = async (formData: FormData) => {
@@ -76,30 +62,7 @@ export default async function Login({
 
     const email = formData.get("email") as string
     const password = formData.get("password") as string
-    /*
-    const emailDomainWhitelistPatternsString = await getEnvVarOrEdgeConfigValue(
-      "EMAIL_DOMAIN_WHITELIST"
-    )
-    const emailDomainWhitelist = emailDomainWhitelistPatternsString?.trim()
-      ? emailDomainWhitelistPatternsString?.split(",")
-      : []
-    const emailWhitelistPatternsString =
-      await getEnvVarOrEdgeConfigValue("EMAIL_WHITELIST")
-    const emailWhitelist = emailWhitelistPatternsString?.trim()
-      ? emailWhitelistPatternsString?.split(",")
-      : []
 
-    // If there are whitelist patterns, check if the email is allowed to sign up
-    if (emailDomainWhitelist.length > 0 || emailWhitelist.length > 0) {
-      const domainMatch = emailDomainWhitelist?.includes(email.split("@")[1])
-      const emailMatch = emailWhitelist?.includes(email)
-      if (!domainMatch && !emailMatch) {
-        return redirect(
-          `/login?message=Email ${email} is not allowed to sign up.`
-        )
-      }
-    }
-*/
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
 
