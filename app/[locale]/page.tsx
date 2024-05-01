807fc991c54cf77fdde7993c02a295b3bb7a4e8f
SamIron0 <samuelironkwec@gmail.com>
2024-04-30 21:56:29 -0500

    x

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,14 +1,6 @@
 "use client"
-import Dash from "@/components/adminDashboard/dash"
-import { ChatInput } from "@/components/chat/chat-input"
-import { Brand } from "@/components/ui/brand"
-import { ChatbotUIContext } from "@/context/context"
-import { getGuestForYou } from "@/db/for-you"
+
 import { createClient } from "@/lib/supabase/client"
-import { TablesInsert } from "@/supabase/types"
-import axios from "axios"
-import { useTheme } from "next-themes"
-import { useRouter } from "next/navigation"
 
 export default async function ChatPage() {
   const supabase = createClient()
---
+++ app/layout.tsx
@@ -0,0 +1,115 @@
+import { Toaster } from "@/components/ui/sonner"
+import { Providers } from "@/components/utility/providers"
+import { Database } from "@/supabase/types"
+import { createServerClient } from "@supabase/ssr"
+import { Metadata, Viewport } from "next"
+import { Inter } from "next/font/google"
+import { cookies } from "next/headers"
+import { ReactNode } from "react"
+import "./globals.css"
+import { Analytics } from "@vercel/analytics/react"
+import { Dashboard } from "@/components/ui/dashboard"
+
+const inter = Inter({ subsets: ["latin"] })
+const APP_NAME = "Fitpal AI"
+const APP_DEFAULT_TITLE = "Fitpal AI"
+const APP_TITLE_TEMPLATE = "%s - Fitpal AI"
+const APP_DESCRIPTION = "Chabot UI PWA!"
+
+interface RootLayoutProps {
+  children: ReactNode
+  params: {
+    locale: string
+  }
+}
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black",
+    title: APP_DEFAULT_TITLE
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE
+    },
+    description: APP_DESCRIPTION
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE
+    },
+    description: APP_DESCRIPTION
+  }
+}
+
+export const viewport: Viewport = {
+  themeColor: "#000000"
+}
+
+const i18nNamespaces = ["translation"]
+
+export default async function RootLayout({
+  children,
+  params: { locale }
+}: RootLayoutProps) {
+  const cookieStore = cookies()
+  const supabase = createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        }
+      }
+    }
+  )
+  const session = (await supabase.auth.getSession()).data.session
+
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DM7XC7YDQT"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-DM7XC7YDQT');
+              `
+          }}
+        ></script>
+      </head>
+      <body className={inter.className}>
+        <Providers attribute="class" defaultTheme="dark">
+          <Toaster richColors position="top-center" duration={3000} />
+          <div className="flex flex-col items-center overflow-x-hidden bg-black text-foreground">
+            {children}
+          </div>
+          <Analytics />
+        </Providers>
+      </body>
+    </html>
+  )
+}
