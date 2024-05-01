786f4260cafde9c7b56395e05bcc73f88b43b5f3
SamIron0 <samuelironkwec@gmail.com>
2024-05-01 15:54:06 -0500

    z

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -110,7 +110,7 @@ export default async function Login({
   }
 
   return (
-    <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">
+    <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 pt-12 sm:max-w-md">
       <form
         className="flex w-full flex-1 flex-col justify-center gap-2 text-foreground animate-in"
         action={signIn}
