274e8f1ce3042368fb8c4b4db810ade840502830
SamIron0 <samuelironkwec@gmail.com>
2024-05-02 14:26:54 -0500

    x

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -23,42 +23,6 @@ interface RootLayoutProps {
   }
 }
 
-export const metadata: Metadata = {
-  applicationName: APP_NAME,
-  title: {
-    default: APP_DEFAULT_TITLE,
-    template: APP_TITLE_TEMPLATE
-  },
-  description: APP_DESCRIPTION,
-  manifest: "/manifest.json",
-  appleWebApp: {
-    capable: true,
-    statusBarStyle: "black",
-    title: APP_DEFAULT_TITLE
-    // startUpImage: [],
-  },
-  formatDetection: {
-    telephone: false
-  },
-  openGraph: {
-    type: "website",
-    siteName: APP_NAME,
-    title: {
-      default: APP_DEFAULT_TITLE,
-      template: APP_TITLE_TEMPLATE
-    },
-    description: APP_DESCRIPTION
-  },
-  twitter: {
-    card: "summary",
-    title: {
-      default: APP_DEFAULT_TITLE,
-      template: APP_TITLE_TEMPLATE
-    },
-    description: APP_DESCRIPTION
-  }
-}
-
 export const viewport: Viewport = {
   themeColor: "#000000"
 }
