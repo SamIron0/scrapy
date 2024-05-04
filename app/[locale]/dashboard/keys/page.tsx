89ee83270f2f373988b429a3aac1532e2b9cfff9
SamIron0 <samuelironkwec@gmail.com>
2024-05-03 20:35:44 -0500

    x

--- app/[locale]/dashboard/keys/page.tsx
+++ app/[locale]/dashboard/keys/page.tsx
@@ -6,14 +6,15 @@ import { createClient } from "@/lib/supabase/client"
 
 import { useRouter } from "next/navigation"
 import { ChatbotUIContext } from "@/context/context"
+import { toast } from "sonner"
 
 interface Props {}
 
 export default function ApiKeys() {
   const supabase = createClient()
   const { apikeys, schema } = useContext(ChatbotUIContext)
-  const [copied, setCopied] = useState(false)
-
+  const [api_key_copied, set_api_key_copied] = useState(false)
+  const [schema_key_copied, setSchema_key_copied] = useState(false)
   const router = useRouter()
   useEffect(() => {
     async function checkUser() {
@@ -29,14 +30,15 @@ export default function ApiKeys() {
     checkUser()
   }, [])
 
-  const handleCopyClick = () => {
+  const handleApiKeyClick = () => {
     if (apikeys) {
       navigator.clipboard
         .writeText(apikeys.id.toString())
         .then(() => {
-          setCopied(true)
+          toast.success("Copied to clipboard")
+          set_api_key_copied(true)
           setTimeout(() => {
-            setCopied(false)
+            set_api_key_copied(false)
           }, 1000)
         })
         .catch(error => {
@@ -44,7 +46,22 @@ export default function ApiKeys() {
         })
     }
   }
-
+  const handleCopySchemaClick = () => {
+    if (schema) {
+      navigator.clipboard
+        .writeText(schema.id.toString())
+        .then(() => {
+          toast.success("Copied to clipboard")
+          setSchema_key_copied(true)
+          setTimeout(() => {
+            setSchema_key_copied(false)
+          }, 1000)
+        })
+        .catch(error => {
+          console.error("Error copying text to clipboard:", error)
+        })
+    }
+  }
   return (
     <div className="mt-10 flex w-full flex-col items-center  overflow-y-auto p-6 sm:mt-0 sm:p-16">
       <div className="flex w-full max-w-3xl flex-col items-center justify-center">
@@ -53,27 +70,9 @@ export default function ApiKeys() {
         <div className="flex  flex-col items-center justify-center rounded-lg border border-input p-4">
           <span className="flex flex-row">
             <p className="mr-6">{apikeys?.id}</p>
-            <svg
-              fill={copied ? "#66d37e" : "none"}
-              stroke={copied ? "currentColor" : "none"}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              viewBox="0 0 24 24"
-              height="1em"
-              width="1em"
-              onClick={handleCopyClick}
-            >
-              <path
-                d="M11 9 H20 A2 2 0 0 1 22 11 V20 A2 2 0 0 1 20 22 H11 A2 2 0 0 1 9 20 V11 A2 2 0 0 1 11 9 z"
-                className={copied ? "copied" : ""}
-              />
-              <path
-                d="M5 15H4a2 2 0 01-2-2V4a2 2 0 012-2h9a2 2 0 012 2v1"
-                opacity={copied ? 0.5 : 1}
-                className={copied ? "copied" : ""}
-              />
-            </svg>
+            <Button size={"sm"} onClick={handleApiKeyClick}>
+              {api_key_copied ? "copied" : "copy"}
+            </Button>
           </span>
         </div>
         <p className="mb-2 w-full pt-12  text-2xl">SCHEMA KEYS</p>
@@ -81,8 +80,12 @@ export default function ApiKeys() {
         <div className="flex  flex-col items-center justify-center rounded-lg border border-input p-4">
           <span className="flex flex-row">
             <p className="mr-6">{schema?.id}</p>
-            <Button size={"sm"} className="ml-auto">
-              copy
+            <Button
+              size={"sm"}
+              className="ml-auto"
+              onClick={handleCopySchemaClick}
+            >
+              {schema_key_copied ? "copied" : "copy"}
             </Button>
           </span>
         </div>
