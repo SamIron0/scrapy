54883ed616eadcad474521516cf64d6b6a73255e
SamIron0 <samuelironkwec@gmail.com>
2024-05-04 19:21:09 -0500

    x

--- app/[locale]/dashboard/keys/page.tsx
+++ app/[locale]/dashboard/keys/page.tsx
@@ -64,30 +64,26 @@ export default function ApiKeys() {
   }
   return (
     <div className="mt-10 flex w-full flex-col items-center  overflow-y-auto p-6 sm:mt-0 sm:p-16">
-      <div className="flex w-full max-w-3xl flex-col items-center justify-center">
+      <div className="flex w-full max-w-3xl flex-col p-2 ">
         <p className="mb-2 w-full text-2xl">API KEYS</p>
 
-        <div className="flex  flex-col items-center justify-center rounded-lg border border-input p-4">
-          <span className="flex flex-row">
-            <p className="mr-6">{apikeys?.id}</p>
-            <Button size={"sm"} onClick={handleApiKeyClick}>
-              {api_key_copied ? "copied" : "copy"}
-            </Button>
-          </span>
+        <div className="flex w-full max-w-xl flex-row items-center justify-center rounded-lg border border-input p-4">
+          <p className="mr-6">{apikeys?.id}</p>
+          <Button size={"sm"} onClick={handleApiKeyClick}>
+            {api_key_copied ? "copied" : "copy"}
+          </Button>
         </div>
         <p className="mb-2 w-full pt-12  text-2xl">SCHEMA KEYS</p>
 
-        <div className="flex  flex-col items-center justify-center rounded-lg border border-input p-4">
-          <span className="flex flex-row">
-            <p className="mr-6">{schema?.id}</p>
-            <Button
-              size={"sm"}
-              className="ml-auto"
-              onClick={handleCopySchemaClick}
-            >
-              {schema_key_copied ? "copied" : "copy"}
-            </Button>
-          </span>
+        <div className="flex  w-full max-w-xl flex-row items-center justify-center rounded-lg border border-input p-4">
+          <p className="mr-6">{schema?.id}</p>
+          <Button
+            size={"sm"}
+            className="ml-auto"
+            onClick={handleCopySchemaClick}
+          >
+            {schema_key_copied ? "copied" : "copy"}
+          </Button>
         </div>
       </div>
     </div>
