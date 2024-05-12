1ee2c5dff95e591af8fa1f2676653103edff1ebd
SamIron0 <samuelironkwec@gmail.com>
2024-05-11 19:04:09 -0600

    x

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -341,7 +341,7 @@ export default function Dash() {
               ) : (
                 <div className="relative">
                   <button
-                    className="absolute right-0 top-0 p-2"
+                    className="absolute right-2 top-2 p-2"
                     onClick={handleCopySchemaClick}
                   >
                     {schema_json_copied ? (
